f52c5cb7c6a7b1ef5e3e2b59585d988970c50057
AnjaConev <anjaconev@gmail.com>
2026-01-30 12:29:46 +0000

    frontend templates and api points

--- tutorial_project/proteins/urls.py
+++ tutorial_project/proteins/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("search", views.prot_autocomplete, name="search"),
+    path("show", views.prot_show, name="show")
 ]
--- tutorial_project/proteins/views.py
+++ tutorial_project/proteins/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.core import serializers
+from django.http import JsonResponse
+
+from .models import Protein
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to the proteins app", )
+    #return HttpResponse("Welcome to the proteins app", )
+    return render(request, "proteins/index.html", context={})
+
+def prot_autocomplete(request):
+    q = request.GET.get("q").upper()
+    print(q)
+    proteins = Protein.objects.filter(uniprot_id__startswith=q).all()
+    uniprot_ids = [p.uniprot_id for p in proteins]
+    print(uniprot_ids)
+    return JsonResponse({"uniprot_ids": uniprot_ids})
+
+def prot_show(request):
+    q = request.GET.get("q").upper()
+    print(q)
+    protein = Protein.objects.filter(uniprot_id=q).first()
+    print(protein)
+    return render(request, "proteins/show_table.html", context = {"protein": protein})
